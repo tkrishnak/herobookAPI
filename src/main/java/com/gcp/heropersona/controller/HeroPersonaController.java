bada52a839bcd65dc7a7e57ce82cfcee8ec1588a
tkrishnak <tkrishnagokul@gmail.com>
2021-01-14 19:38:49 -0500

    Refactor code to remove all the unused imports and other non used codes

--- src/main/java/com/gcp/heropersona/controller/HeroPersonaController.java
+++ src/main/java/com/gcp/heropersona/controller/HeroPersonaController.java
@@ -11,44 +11,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/gcp/api")
 public class HeroPersonaController {
-    private List<Hero> heroList;
-    private List<Villain> villainList;
 
     private HeroPersonaService heroPersonaService;
 
     public HeroPersonaController(HeroPersonaService heroPersonaService) {
         this.heroPersonaService = heroPersonaService;
     }
-
-    /*public HeroPersonaController() {
-        Hero hero = new Hero("/src/batman.jpg","Bruce Wayne","Batman","6 feet","180 lbs",
-        "None", "100", "90", "None", "Normal","60",
-                "Fighting crime at night wearing bat suit", "Turned into batman to make the gotham city safer");
-        heroList = new ArrayList<>();
-        heroList.add(hero);
-        hero = new Hero("SpiderMan");
-        heroList.add(hero);
-        hero = new Hero("SuperMan");
-        heroList.add(hero);
-
-        Villain villain = new Villain("Joker","Batman","/src/joker.jpg","Jack Napier","Batman","6 feet","180 lbs",
-                "None", "200", "60", "None", "Fast","70",
-                "Fighting Batman", "Fight Batman with all his evil ideas");
-        villainList = new ArrayList<>();
-        villainList.add(villain);
-        villain = new Villain("Dr.Octopus");
-        villainList.add(villain);
-        villain = new Villain("Lex Luther");
-        villainList.add(villain);
-    }*/
 
     @GetMapping("/heros")
     public List<Hero> getAllHeros(){
@@ -76,12 +50,7 @@ public class HeroPersonaController {
         if(villain.isPresent()) {
             return ResponseEntity.ok().body(villain);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Hero doesn't exist");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Villain doesn't exist");
         }
-        //List<Hero> myVillain =  villainList.stream().filter(villain -> villain.getVillainName().equalsIgnoreCase(name)).collect(Collectors.toList());
-        //if(myVillain != null && myVillain.size()>0)
-          //  return ResponseEntity.ok().body(myVillain.get(0));
-        //else
-          //  return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Villain doesn't exist");
     }
 }
